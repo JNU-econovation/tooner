132e68bc19bc399baf2f8699a62d3b4dee656f0d
Andy H. Jung <commaniakr@gmail.com>
2019-11-11 21:42:59 +0900

    추가: express 서버 테스트 코드

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const passport = require('passport');
+
+
+// 로깅 모듈 
+app.use(morgan('combined'));
 
 app.get("/", function(req, res) {
     res.send("<big>Hello Tooners!</big>");
 })
 
+app.get("/tnrlogin/", function(req, res) {
+    
+})
+
 
 
 var server = app.listen(80, function() {
-    console.log("Tooner Since 2019! 서버가 켜지고 있습니다.");
+    console.log("==============================================\nTooner Since 2019! Server is turning on......\n==============================================");
 })
