e89dcbdfc7c799199e843fc4227d6d87cd3545ee
SEONGYEONG <mtoc227@gmail.com>
2019-11-28 23:29:17 +0900

    modify: forward Interactive Web

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -6,16 +6,16 @@ import {Link} from 'react-router-dom';
 function Header() {
     return (
       <header>
-        <div className="banner-container inline">
+        <div className="banner-container">
           <Link to="/">
             <img id="banner" src="./banner.png" alt=""/>
           </Link>
         </div>
-        <div className="userButton inline">
+        <div className="user-container">
             <Link to="/login">
                 <button className="login">로그인</button>
             </Link>
-            <Link to="/sign">
+            <Link to="/signup">
                 <button className="sign">회원가입</button>
             </Link>
         </div>
