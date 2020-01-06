df64837666ef724ced18a5feba5fb71316bb9ed1
SEONGYEONG <mtoc227@gmail.com>
2020-01-06 21:15:54 +0900

    add: /Board/Webtoon/

---
+++ frontend/src/components/Board/Webtoon/Webtoon.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Webtoon.css';
+
+function Webtoon() {
+    return(
+        <div className="board-container">
+            웹툰이야기 게시판
+        </div>
+    );
+}
+
+export default Webtoon;
