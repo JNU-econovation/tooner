e545b58ea9bb1e774e5237656aa9bfeadf28795c
SEONGYEONG <mtoc227@gmail.com>
2020-01-07 23:40:08 +0900

    modify: some changes

---
+++ frontend/src/components/Board/Notice.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Notice.css'
+
+function Notice() {
+    return(
+        <div className="notice">
+            공지사항 부분
+        </div>
+    );
+}
+
+export default Notice;
