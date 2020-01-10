8ada9a7f33fe475a412bf86a74e105ff20eec619
SEONGYEONG <mtoc227@gmail.com>
2020-01-10 16:49:22 +0900

    modify: css

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -2,31 +2,53 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Post.css';
 
-function Post({ writeralias, title, writetime, edittime, hit, like }) {
+function Post({ writeralias, title, content, writetime, edittime, hit, like, dislike }) {
     var isEdited = edittime == null ? false : true;
     return(
-        <section className="post-container">
-            <div>{writeralias}</div>
-            <div>{title}</div>
-            <div>{writetime}</div>
-            { isEdited ? (
-                <div>{edittime}</div>
-            ) : (
-                <div></div>
-            )}
-            <div>{hit}</div>
-            <div>{like}</div>
-        </section>
+        <div className="post-container">
+            <div id="title-wrap">
+                <span id="post-title">{title}</span>
+            </div>
+            <div id="post-info">
+                <ul>작성자</ul>
+                    <li>{writeralias}</li>
+                <ul>작성일</ul>
+                    <li>{writetime}</li>
+                <ul>수정일</ul>
+                    { isEdited ? (
+                        <li>{edittime}</li>
+                    ) : (
+                        <li>{writetime}</li>
+                    )}
+                <ul>조회수</ul>
+                    <li>{hit}</li>
+            </div>
+            <div id="content-wrap">
+                {content}
+            </div>
+            <div id="like-container">
+                <div id="like">
+                    <i className="far fa-thumbs-up"></i>
+                    {like}
+                </div>
+                <div id="dislike">
+                    <i class="far fa-thumbs-down"></i>
+                    {dislike}
+                </div>
+            </div>
+        </div>
     );
 }
 
 Post.propTypes = {
     writeralias: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired,
     writetime: PropTypes.string.isRequired,
     edittime: PropTypes.string.isRequired,
     hit: PropTypes.number.isRequired,
-    like: PropTypes.number.isRequired
+    like: PropTypes.number.isRequired,
+    dislike: PropTypes.number.isRequired
 }
 
 export default Post;
