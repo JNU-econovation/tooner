a61bd0fc6336c01a0ce8ab1f9e653eb117f57c83
SEONGYEONG <mtoc227@gmail.com>
2020-01-12 15:31:00 +0900

    modify: detail

--- frontend/src/components/Review/WriteReviewPage/WriteReviewPage.js
+++ frontend/src/components/Review/WriteReviewPage/WriteReviewPage.js
@@ -1,75 +1,129 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import StarRating from 'react-svg-star-rating';
 import { useHistory } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import './WriteReviewPage.css';
 import axios from 'axios';
 
 function WriteReviewPage(props) {
-    var json = "http://168.131.30.129:2599/longreview" //props.location.json;
-    console.log(json);
-    const [data, setData] = useState({
+    var json = props.json;
+    var prefList = ['스토리', '캐릭터', '작화', '연출'];
+    var isGood = new Array(prefList.length).fill(false);
+    var isBad = new Array(prefList.lenght).fill(false);
+    var data = {
         title: '',
         rating: -1,
         preference: -1,
         good: [],
         bad: [],
         content: ''
-    });
+    }
     const { handleSubmit } = useForm();
     const history = useHistory();
+
     const [status, setStatus] = useState(200);
+    const [empty, setEmpty] = useState(false);
+    
+    const onChange = e => {
+        data[e.target.name] = e.target.value;
+    }
+    const onChangeGood = e => {
+        isGood[e.target.name] = !isGood[e.target.name];
+    }
+    const onChangeBad = e => {
+        isBad[e.target.name] = !isBad[e.target.name];
+    }
+    const onChangeRating = rating => {
+        data.rating = rating;
+    }
+    const onChangePref = e => {
+        data.preference = parseInt(e.target.value);
+    }
+
+    const prefGoodElement = [], prefBadElement = [];
+    for(let i=0; i<prefList.length; i++) {
+        prefGoodElement.push(
+            <label>
+                <input type="checkbox" name={i} onChange={onChangeGood} /> {prefList[i]}
+            </label>
+        )
+        prefBadElement.push(
+            <label>
+                <input type="checkbox" name={i} onChange={onChangeBad} /> {prefList[i]}
+            </label>
+        )
+    }
+
     const onSubmit = () => {
-        console.log(data);
-        axios.post(json, data)
-        .then(res => {
-            console.log(res);
-            setStatus(res.status);
-            if(status === 200) history.goBack();
-        }).catch(err => {
-            setStatus(401);
-        });
+        if(data.title !== '' && data.rating !== -1 && data.preference !== -1) {
+            setEmpty(false)
+            for(let i=0; i<prefList.length; i++) {
+                isGood[i] && data.good.push(prefList[i]);
+                isBad[i] && data.bad.push(prefList[i]);
+            }
+            console.log(data);
+            console.log(json);
+            axios.post(json, data)
+            .then(res => {
+                console.log(res);
+                setStatus(res.status);
+                if(status === 200) history.goBack();
+            }).catch(err => {
+                setStatus(401);
+            });
+        } else {
+            setEmpty(true);
+        }
     };
-    const onChangeTitle = e => {
-        data['title'] = e.target.value;
-    }
-    const onChangeReviewTitle = e => {
-        data['reviewtitle'] = e.target.value;
-    }
-    const onChangeContent = e => {
-        data['content'] = e.target.value;
-    }
-    useEffect( () => {
-        setData(data);
-    })
+
     return(
         <form className="page-container" onSubmit={handleSubmit(onSubmit)}>
-            <div className="title-container">
-                <span id="page-title">상세 리뷰 쓰기</span>
-                <div id="webtoon-title">
-                    <input type="text" name="title" placeholder="리뷰할 웹툰" onChange={onChangeTitle} />
-                    <span>{data.title}</span>
+            {
+                status === 401 ? (
+                    <p id="message">* 등록에 실패하였습니다.</p>
+                ) : (
+                    null
+                )
+            }
+            <div className="search-title search-margin">
+                <p>리뷰할 웹툰을 선택해주세요.</p>
+                <input type="text" name="title" onChange={onChange} />
+            </div>
+            <div className="search-rating search-margin">
+                <p>이 웹툰을 평가해주세요.</p>
+                <StarRating
+                    handleOnClick={onChangeRating}
+                />
+            </div>
+            <div className="search-pref search-margin">
+                <p>이 웹툰은 취향에 맞나요?</p>
+                <div id="radio-container">
+                    <input type="radio" value="2" id="o" name="preference"  onChange={onChangePref} />
+                        <label htmlFor="o">취향이다</label>
+                    <input type="radio" value="1" id="ox" name="preference" onChange={onChangePref} />
+                        <label htmlFor="ox">그저 그렇다</label>
+                    <input type="radio" value="0" id="x" name="preference" onChange={onChangePref} />
+                        <label htmlFor="x">취향이 아니다</label>
                 </div>
-                <div id="review-title">
-                    <input type="text" name="reviewtitle" placeholder="리뷰 제목을 써주세요." onChange={onChangeReviewTitle} />
-                </div>
-                <div id="review-rating">
-                    <input type="text" name="rating" />
-                </div>
-                <div id="review-pref">
-                    <input type="text" name="preference" />
-                </div>
-                <div id="review-good">
-                    <input type="text" name="good" />
-                </div>
-                <div id="review-bad">
-                    <input type="text" name="bad" />
-                </div>
-                <div id="review-content">
-                    <input type="text" name="content" placeholder="리뷰 내용을 써주세요." onChange={onChangeContent} />
-                </div>
-                <div id="button-container">
-                    <button type="submit" id="submit">올리기</button>
-                </div>
+            </div>
+            <div className="search-good search-margin search-label">
+                <p>이 웹툰에서 좋았던 점은?</p>
+                {prefGoodElement}
+            </div>
+            <div className="search-bad search-margin search-label">
+                <p>이 웹툰에서 아쉬운 점은?</p>
+                {prefBadElement}
+            </div>
+            {
+                empty ? (
+                    <p id="warning">* 웹툰 제목, 평점, 취향을 선택해주세요.</p>   
+                ) : (
+                    null
+                )
+            }
+            <textarea name="content" id="content" onChange={onChange} />
+            <div id="submit-wrap">
+                <button type="submit" id="submit">올리기</button>
             </div>
         </form>
     );
