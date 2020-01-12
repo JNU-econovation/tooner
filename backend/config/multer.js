12ac0278f0f46fb8244477162f9aef4a53f1a428
Andy H. Jung <commaniakr@gmail.com>
2020-01-12 20:01:48 +0900

    refactor: .gitignore, multer 예외처리

--- backend/config/multer.js
+++ backend/config/multer.js
@@ -3,16 +3,22 @@ const multer = require('multer');
 var multerConfig = {
     storage: multer.diskStorage({
         destination: function(req, file, cb) {
-            //이미지 파일이면
-            if (file.mimetype == "image/jpeg" || file.mimetype == "image/jpg" || file.mimetype == "image/png") {
-                cb(null, 'uploads/images')
-            }
+            cb(null, 'uploads/images')
         },
         //파일 이름 설정
         filename: function(req, file, cb) {
             cb(null, Date.now() + "-" + file.originalname)
         }
-    })
+    }),
+    fileFilter: function(req, file, cb) {
+        if (file.mimetype != "image/jpeg" && file.mimetype != "image/jpg" && file.mimetype != "image/png") {
+            return cb(new Error("이미지만 업로드 가능합니다."));
+        };
+        cb(null, true);
+    },
+    limits: {
+        fileSize: 4096 * 4096
+    }
 }
 
 module.exports = multerConfig;
