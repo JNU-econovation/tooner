9de5181714e24c9678a1df7e810c27ce67fd2b36
SEONGYEONG <mtoc227@gmail.com>
2020-01-13 23:58:41 +0900

    add: context folders

---
+++ frontend/src/components/Context/AuthProvider.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import AuthHelperMethods from './AuthHelperMethods';
+
+const auth = new AuthHelperMethods();
+const AuthContext = React.createContext(null);
+const initialState = {
+    logged: auth.loggedIn(),
+    token: auth.getToken()
+}
+
+const reducer = (state, action) => {
+    switch(action.type) {
+        case 'login':
+            const token = action.payload.token;
+            localStorage.setItem('token', JSON.stringify(token));
+            return {
+                ...state,
+                logged: true,
+                token: token
+            };
+        case 'logout':
+            localStorage.clear();
+            return {
+                ...state,
+                logged: false,
+                token: null
+            }
+        default:
+            return state;
+    }
+}
+
+const AuthProvider = ({ children }) => {
+    const [state, dispatch] = React.useReducer(reducer, initialState);
+    console.log(state);
+    return(
+        <AuthContext.Provider value={{
+            state,
+            dispatch
+        }}>
+            { children }
+        </AuthContext.Provider>
+    )
+}
+
+export { AuthProvider, AuthContext };
