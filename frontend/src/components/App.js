3b489d2a1ecc141b96f1c1f2b0bf5e70d751d1a7
SEONGYEONG <mtoc227@gmail.com>
2020-01-13 23:53:13 +0900

    modify: Routes.js to App.js

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,12 +1,42 @@
-import React from 'react';
-import Routes from './Routes';
+import React, { useContext } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-class App extends React.Component {
-  render() {
-    return (
-      <Routes />
-    );
-  }
+import Main from './Main/Main';
+import Header from './Header/Header';
+import Footer from './Footer/Footer';
+import MiddleHeader from './MiddleHeader/MiddleHeader'
+import Login from './Login';
+import Signup from './Signup';
+import MyPage from './User/MyPage/MyPage';
+import ShortReview from './Review/ShortReview/ShortReview';
+import LongReview from './Review/LongReview/LongReview';
+import WebtoonPage from './Board/Webtoon/WebtoonPage';
+import ThreadPage from './Thread/ThreadPage';
+
+function App() {
+  return(
+    <Router>
+        <Header />
+        <Switch>
+            <Route path='/login'>
+              <Login />
+            </Route>
+            <Route path='/signup' component={Signup} />
+            <Route path='/mypage' component={MyPage} />
+            <Route path='/' component={MiddleHeader} />
+        </Switch>
+        <Switch>
+            <Route path='/review/short' component={ShortReview} />
+            <Route path='/review/long' component={LongReview} />
+            <Route path='/review' component={ShortReview} />
+            <Route path='/board/webtoon' component={WebtoonPage} />
+            <Route path='/thread' component={ThreadPage} />
+            <Route exact path='/' component={Main} />
+            {/*<Route component={NotFound} />*/}
+        </Switch>
+        <Footer />
+    </Router>
+  );
 }
 
-export default App;
+export default App
