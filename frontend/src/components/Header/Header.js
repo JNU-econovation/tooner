3996c9159f4faa8f9151e52a1ca72fb671ba316a
SEONGYEONG <mtoc227@gmail.com>
2020-01-13 23:56:19 +0900

    add: login or logout status and context

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,41 +1,56 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 import './Header.css'
-import {Link} from 'react-router-dom';
-
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../Context/AuthProvider';
 
 function Header() {
-    return (
-      <header>
-        <div className="banner-container">
-          <Link to="/" id="banner">
-            tooner
-          </Link>
-        </div>
-        <div className="user-container">
-          <Link to="/review/short">
-            <button>한줄 리뷰</button>
-          </Link>
-          <Link to="/review/long">
-            <button>상세 리뷰</button>
-          </Link>
-          {/*<Link to="/">
-            <button>인기글</button>
-          </Link>*/}
-          <Link to="/board/webtoon">
-            <button>웹툰이야기</button>
-          </Link>
-          <Link to="/thread">
-            <button>스레드</button>
-          </Link>
-          <Link to="/login">
-              <button className="login">로그인</button>
-          </Link>
-          <Link to="/signup">
-              <button className="sign">회원가입</button>
-          </Link>
-        </div>
-      </header>
-    );
+  const { state, dispatch } = useContext(AuthContext);
+  return (
+    <header>
+      <div className="banner-container">
+        <Link to="/" id="banner">
+          tooner
+        </Link>
+      </div>
+      <div className="user-container">
+        <Link to="/review/short">
+          <button>한줄 리뷰</button>
+        </Link>
+        <Link to="/review/long">
+          <button>상세 리뷰</button>
+        </Link>
+        {/*<Link to="/">
+          <button>인기글</button>
+        </Link>*/}
+        <Link to="/board/webtoon">
+          <button>웹툰이야기</button>
+        </Link>
+        <Link to="/thread">
+          <button>스레드</button>
+        </Link>
+        {
+          !state.logged ? (
+            <React.Fragment>
+              <Link to="/login">
+                <button className="login">로그인</button>
+              </Link>
+              <Link to="/signup">
+                <button className="sign">회원가입</button>
+              </Link>
+            </React.Fragment>
+          ) : (
+            <Link to="/">
+              <button
+                className="logout"
+                onClick={() => dispatch({
+                type: 'logout'
+              })}>로그아웃</button>
+            </Link>
+          )
+        }
+      </div>
+    </header>
+  );
 }
   
 export default Header
