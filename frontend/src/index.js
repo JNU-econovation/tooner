5d19a37872d6cc650742d4780ef16b7e8235c444
SEONGYEONG <mtoc227@gmail.com>
2020-01-13 23:52:02 +0900

    modify: component structure

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { AuthProvider }  from '../src/components/Context/AuthProvider';
+
+function Index() {
+    console.log(localStorage)
+    return(
+        <AuthProvider>
+            <App />
+        </AuthProvider>
+    )
+}
+
+ReactDOM.render(<Index />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
