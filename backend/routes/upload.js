8272adfac17dfe18afb3e1409dd7446e433e9fe7
Andy H. Jung <commaniakr@gmail.com>
2020-01-15 21:42:34 +0900

    refactor: 파일 업로드 테스트

--- backend/routes/upload.js
+++ backend/routes/upload.js
@@ -5,6 +5,7 @@ const upload = multer(multerconfig);
 module.exports = function(app) {
     app.post('/upload/image', upload.single('file'), function(req,res) {
         if(!req.file) throw("파일이 없습니다.");
+        console.log(req.file);
         res.json({ message: "success", data: [req.file.filename]});
     });
 };
--- backend/routes/userdb.js
+++ backend/routes/userdb.js
@@ -3,12 +3,23 @@ var { User } = require('../models');
 
 module.exports = function(app) {
     app.get('/userinfo', isLoggedIn, function(req,res) {
-        console.log(req.user.user_no);
         User.findOne({
             where: {user_no: req.user.user_no},
             attributes: ['username', 'alias', 'verify_code', 'favorite_genre']
         }).then(user => {
-            console.log(user);
+            res.json({message: "Success", data: user.dataValues});
+        }).catch(function (err) {
+            res.status(500).json({ message: "Fail", exception: err });
+        });
+    })
+
+    app.put('/userinfo', isLoggedIn, function(req,res) {
+        User.update({
+            alias: req.body.alias,
+            favorite_genre: req.body.favorite_genre
+        }, {
+            where: {user_no: req.user.user_no}
+        }).then(user => {
             res.json({message: "Success", data: user.dataValues});
         }).catch(function (err) {
             res.status(500).json({ message: "Fail", exception: err });
