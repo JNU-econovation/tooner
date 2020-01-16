e454b60574b05b042405ef04f9cc8f4fdbef7244
SEONGYEONG <mtoc227@gmail.com>
2020-01-16 16:35:01 +0900

    feat: modify modal component

--- frontend/src/components/Review/ShortReview/ShortReview.js
+++ frontend/src/components/Review/ShortReview/ShortReview.js
@@ -1,34 +1,47 @@
 import React, { useState, useEffect } from 'react';
+import { Route } from 'react-router-dom';
 import '../../SinglePage.css';
 import Sort from '../Sort';
 import WriteReview from './WriteReview';
 import ReviewBox from '../ReviewBox';
-import ShortReviewGuide from '../../Modal/ShortReviewGuide/ShortReviewGuide';
+import ReviewGuide from '../../Modal/ReviewGuide/ReviewGuide';
 
-function ShortReview() {
-    const url = "http://168.131.30.129:2599/shortreview";
-    const [state, setState] = useState({
-        isModalOpen: false
-    })
-    const openModal = () => {
-        setState({ isModalOpen: true });
+function ShortReview({ match }) {
+    const api = "http://168.131.30.129:2599/shortreview";
+
+    const [isGuideOpen, setGuide] = useState(false);
+    const openGuide = () => {
+        setGuide(true);
     }
-    const closeModal = () => {
-        setState({ isModalOpen: false });
+    const closeGuide = () => {
+        setGuide(false);
     }
+
+    var content =
+    <div id="guide-detail">
+        한줄 리뷰입니다!
+        아주아주 깨끗한 리뷰 문화를 만들어가요 ^▽^
+    </div>
+
     useEffect(() => {
     });
     return(
         <div className="page-container">
             <div className="title-container">
                 <span id="page-title">한줄 리뷰</span>
-                <span id="guide" onClick={openModal} >한줄 리뷰 이용 가이드</span>
-                <ShortReviewGuide isOpen={state.isModalOpen} close={closeModal} />
+                <span id="guide" onClick={openGuide} >한줄 리뷰 이용 가이드</span>
+                {
+                    isGuideOpen &&
+                    <ReviewGuide
+                        isOpen={isGuideOpen}
+                        close={closeGuide}
+                        content={content}
+                    />
+                }
             </div>
-            
-            <WriteReview json={url} />
+            <WriteReview api={api} />
             <Sort />
-            <ReviewBox json={url} />
+            <Route path={match.path} component={ReviewBox} />
         </div>
     );
 }
