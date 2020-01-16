acf7d307b61dbb8d8567118b6544b751c0f7a516
SEONGYEONG <mtoc227@gmail.com>
2020-01-16 16:35:52 +0900

    feat: access user's info and edit alias, favorite genre

--- frontend/src/components/User/MyPage/MyPage.js
+++ frontend/src/components/User/MyPage/MyPage.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useCookies } from 'react-cookie';
+
+import UserProfile from './UserProfile';
 import './MyPage.css'
+import axios from 'axios';
+
 
 function MyPage() {
+    const api = "http://168.131.30.129:2599/userinfo";
+
     return(
         <div className="page-container">
             <div className="title-container">
-                <span>마이 페이지</span>
+                <span id="page-title">마이페이지</span>
+                <UserProfile api={api} />
             </div>
+            <br />
         </div>
     )
 }
