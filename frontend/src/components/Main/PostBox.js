9891201225aad4fb627b3244e827a0797bfe41a8
SEONGYEONG <mtoc227@gmail.com>
2020-01-16 22:34:36 +0900

    feat: get request

--- frontend/src/components/Main/PostBox.js
+++ frontend/src/components/Main/PostBox.js
@@ -1,36 +1,74 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 import './PostBox.css';
 import './Main.css'
 
 function SinglePostBox(props) {
   const className = "singleBox " + props.class;
-    return (
-      <div className={className}>
-        <div id="postType">
-          <span>{props.postType}</span>
-        </div>
-        <hr />
-        <div>
-          <ul>
-            <li>글1</li>
-            <li>글2</li>
-            <li>글3</li>
-            <li>글4</li>
-          </ul>
-        </div>
+  const api = "http://168.131.30.129:2599" + props.api;
+  const [posts, setPosts] = useState([
+    {
+      title: '글을 불러오는 중입니다...'
+    },
+    {
+      title: '글을 불러오는 중입니다...'
+    },
+    {
+      title: '글을 불러오는 중입니다...'
+    },
+    {
+      title: '글을 불러오는 중입니다...'
+    }
+  ]);
+
+  useEffect(() => {
+    axios.get(api)
+    .then(res => {
+        setPosts(res.data.data);
+    }).catch(err => {
+      console.log(err);
+    })
+  }, []);
+
+  return (
+    <div className={className}>
+      <div id="postType">
+        <span>{props.postType}</span>
       </div>
-    );
-  }
+      <hr />
+      <div>
+        <ul>
+          {
+            posts.map(post => {
+              return(
+                <Link to={`${props.path}/post/${post.articleid}`}>
+                  {
+                    props.class === "recommendReview" ? (
+                      <li>[{post.title}] {post.reviewtitle}</li>
+                    ) : (
+                      <li>{post.title}</li>
+                    )
+                  }
+                </Link>
+              )
+            })
+          }
+        </ul>
+      </div>
+    </div>
+  );
+}
   
-  function PostBox() {
-    return (
-      <div className="postBox">
-        <SinglePostBox class="notice" postType="공지사항" />
-        <SinglePostBox class="recommendReview" postType="추천 리뷰" />
-        <SinglePostBox class="popularPosts" postType="인기글" />
-        <SinglePostBox class="pleaseFind" postType="찾아주세요" />
-      </div>
-    );
-  }
+function PostBox() {
+  return (
+    <div className="postBox">
+      <SinglePostBox class="notice" api="/boardthumb/notice" path="/board/notice" postType="공지사항" />
+      <SinglePostBox class="recommendReview" api="/toplike/longreview" path="/review/long" postType="추천 리뷰" />
+      <SinglePostBox class="recentPosts" api="/boardthumb/webtoon" path="/board/webtoon" postType="최신글" />
+      <SinglePostBox class="popularPosts" api="/tophit/webtoon" path="/board/webtoon" postType="인기글" />
+    </div>
+  );
+}
 
   export default PostBox;
