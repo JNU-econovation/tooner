721f698cb7756daad283d207d0e31c127a3413f8
SEONGYEONG <mtoc227@gmail.com>
2020-01-17 03:41:48 +0900

    modify: remove search

--- frontend/src/components/MiddleHeader/MiddleHeader.js
+++ frontend/src/components/MiddleHeader/MiddleHeader.js
@@ -51,7 +51,7 @@ function MiddleHeader(props) {
     return (
         <div className="middle-header">
             <HotPost type="hot!!" title="너무너무 귀여운 힐링툰 [롤랑롤랑]" />
-            <Search today="가담항설" btnImageUrl="icon/search.png" />
+            {/*<Search today="가담항설" btnImageUrl="icon/search.png" />*/}
         </div>
     );
 }
