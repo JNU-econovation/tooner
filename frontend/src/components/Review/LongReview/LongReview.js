2bea657729794de8bf4a81fe7ec08b4ff1913ebd
SEONGYEONG <mtoc227@gmail.com>
2020-01-16 16:32:14 +0900

    modify: write review routes

--- frontend/src/components/Review/LongReview/LongReview.js
+++ frontend/src/components/Review/LongReview/LongReview.js
@@ -3,21 +3,21 @@ import { Link, Route } from 'react-router-dom';
 import '../../SinglePage.css';
 import './WriteReview.css';
 import Sort from '../Sort';
-import ReviewBox from '../ReviewBox';
 import WriteReviewPage from '../WriteReviewPage/WriteReviewPage';
 import ReviewGuide from '../../Modal/ReviewGuide/ReviewGuide';
+import Board from './Board';
+import PostPage from './PostPage';
 
 function LongReview({ match }) {
     var title = '좋아하는 웹툰';
-    var url = "http://168.131.30.129:2599/longreview";
-    const [state, setState] = useState({
-        isModalOpen: false
-    })
+    var api = "http://168.131.30.129:2599/longreview";
+    
+    const [isModalOpen, setModal] = useState(false);
     const openModal = () => {
-        setState({ isModalOpen: true });
+        setModal(true);
     }
     const closeModal = () => {
-        setState({ isModalOpen: false });
+        setModal(false);
     }
     var content = <div id="guide-detail">
         상세 리뷰입니다!
@@ -29,11 +29,14 @@ function LongReview({ match }) {
             <div className="title-container">
                 <span id="page-title">상세 리뷰</span>
                 <span id="guide" onClick={openModal}>상세 리뷰 이용 가이드</span>
-                <ReviewGuide
-                    isOpen={state.isModalOpen}
-                    close={closeModal}
-                    content={content}
-                />
+                {
+                    isModalOpen &&
+                    <ReviewGuide
+                        isOpen={isModalOpen}
+                        close={closeModal}
+                        content={content}
+                    />
+                }
             </div>
             <Route exact path={match.path}>
                 <div className="write-review-container">
@@ -41,18 +44,22 @@ function LongReview({ match }) {
                     <span>{title}</span>
                     <Link to={{
                         pathname: `${match.path}/write`,
-                        title: title,
-                        json: url
+                        state: {
+                            api: api,
+                            edit: false
+                        }
                     }}>
                         <button id="go-to-review">리뷰하러 가기</button>
                     </Link>
                 </div>
                 <Sort />
-                <ReviewBox json={url} />
+                <Board location={match.path} json={api} />
             </Route>
-            <Route path={`${match.path}/write`}>
-                <WriteReviewPage json={url}/>
+            <Route path={`${match.path}/write`} component={WriteReviewPage} />
+            <Route path={`${match.path}/post/:articleid`}>
+                <PostPage location={match.path} json={api}/>
             </Route>
+            <Route path={`${match.path}/edit/:articleid`} component={WriteReviewPage} />
         </div>
     );
 }
